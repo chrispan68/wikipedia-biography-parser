44b83af284d00683fa3015e602193ed081c57be6
chrispan68 <chrispan68@gmail.com>
2020-07-21 15:34:49 -0400

    Jasidojasdf

--- parse_wikipedia.py
+++ parse_wikipedia.py
@@ -14,14 +14,14 @@ parser.setContentHandler(handler)
 for line in subprocess.Popen(['bzcat'], 
                               stdin = open(data_path), 
                               stdout = subprocess.PIPE).stdout:
-    begin = len(parser._people)
+    begin = len(handler._people)
     try:
         parser.feed(line)
     except StopIteration:
         break
 
-    if len(parser._people) > begin:
-        print(parser._people[-1][0])
+    if len(handler._people) > begin:
+        print(handler._people[-1][0])
 
 
     
