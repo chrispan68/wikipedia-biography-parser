dc18b20e4d5c38745ef11a909e766d63ef28e96b
chrispan68 <chrispan68@gmail.com>
2020-07-21 13:07:38 -0400

    jaisdjisdf

---
+++ parse_wikipedia.py
@@ -0,0 +1,22 @@
+import wiki-xml-handler
+import xml.sax
+import sys
+
+data_path = sys.argv[1]
+
+# Object for handling xml
+handler = WikiXmlHandler()
+# Parsing object
+parser = xml.sax.make_parser()
+parser.setContentHandler(handler)
+# Iteratively process file
+for line in subprocess.Popen(['bzcat'], 
+                              stdin = open(data_path), 
+                              stdout = subprocess.PIPE).stdout:
+    parser.feed(line)
+    
+    # Stop when 3 articles have been found
+    if len(handler._pages) > 5:
+        for page in handle._pages:
+            print(page[0])
+        break
