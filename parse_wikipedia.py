d738f17c2fd02e44956a61479042da05bc4fb105
chrispan68 <chrispan68@gmail.com>
2020-07-21 13:10:29 -0400

    jasdiasdf

--- parse_wikipedia.py
+++ parse_wikipedia.py
@@ -1,6 +1,7 @@
-import wiki-xml-handler
+from wiki_xml_handler import *
 import xml.sax
 import sys
+import subprocess
 
 data_path = sys.argv[1]
 
@@ -17,6 +18,6 @@ for line in subprocess.Popen(['bzcat'],
     
     # Stop when 3 articles have been found
     if len(handler._pages) > 5:
-        for page in handle._pages:
+        for page in handler._pages:
             print(page[0])
         break
